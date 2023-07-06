e127d7af90b47e88e276cea1cb928acdf8311a82
adhikarisarthak <adhikarisrthk+git@gmail.com>
2023-07-06 15:44:52 -0700

    New changelist

--- fridge_app/models.py
+++ fridge_app/models.py
@@ -1,17 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-# Create your models here.
 class Fridge(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-
 class Item(models.Model):
     name = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
     qty = models.IntegerField()
-    fridge_id = models.ForeignKey(Fridge, on_delete=models.CASCADE)
-    expiry_date = models.DateTimeField()
+    fridge = models.ForeignKey(Fridge, on_delete=models.CASCADE)
+    expiry_date = models.DateField()
+
+    def is_expired(self):
+        return self.expiry_date < datetime.date.today()
+
+    def remove_expired_items(self):
+        Item.objects.filter(expiry_date__lt=datetime.date.today()).delete()
+
+    @staticmethod
+    def search_items(query):
+        return Item.objects.filter(name__icontains=query)
+
+    @staticmethod
+    def generate_shopping_list():
+        items = Item.objects.filter(expiry_date__gte=datetime.date.today())
+        shopping_list = []
+        for item in items:
+            if item.qty > 0:
+                shopping_list.append(item.name)
+                item.qty -= 1
+                item.save()
+        return shopping_list
