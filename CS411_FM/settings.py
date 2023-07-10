2b2aa5946ac8d725d99f59f3cf0526ed5b0d831b
phant <nayanspai@gmail.com>
2023-07-09 17:30:39 -0700

    Added django crispy forms and redesigned sign-up page

--- CS411_FM/settings.py
+++ CS411_FM/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'fridge_app.apps.FridgeAppConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -113,6 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
