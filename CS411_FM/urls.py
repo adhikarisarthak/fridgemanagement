08bfd3e95e9af21ea8ba4f5549d4eaa77b7dd111
phant <nayanspai@gmail.com>
2023-07-16 23:09:31 -0700

    reorganized URL patterns in CS411_FM/urls and users/urls

--- CS411_FM/urls.py
+++ CS411_FM/urls.py
@@ -14,21 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.urls import path, include
+
 from django.conf import settings
 from django.conf.urls.static import static
-from users import views as user_views
+from users import urls
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
-    path('profile/', user_views.profile, name='profile'),
-    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('', include('fridge_app.urls'))
-]
+urlpatterns = urls.urlpatterns
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
